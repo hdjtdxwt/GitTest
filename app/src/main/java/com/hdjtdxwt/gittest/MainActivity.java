5301fff8ed17b56c0fe4e1c1e312d6057efa24ee
wangtao <hdjtdxwt@126.com>
2020-10-30 16:42:52 +0800

    第一个代码修改，加了个btn1

--- app/src/main/java/com/hdjtdxwt/gittest/MainActivity.java
+++ app/src/main/java/com/hdjtdxwt/gittest/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         textView.setOnClickListener(this);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
     }
 
     @Override
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.textView:{
                 Toast.makeText(this, "第一个textView", Toast.LENGTH_SHORT).show();
             } break;
+            case R.id.btn1:{
+                Toast.makeText(this, "按钮1", Toast.LENGTH_SHORT).show();
+            }break;
         }
     }
 }
