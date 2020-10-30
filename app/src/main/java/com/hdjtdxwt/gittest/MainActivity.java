757478e9aac03e020e7d8538926ab2d37e389f4e
wangtao <hdjtdxwt@126.com>
2020-10-30 16:36:53 +0800

    第一次修改添加了一个TextView

--- app/src/main/java/com/hdjtdxwt/gittest/MainActivity.java
+++ app/src/main/java/com/hdjtdxwt/gittest/MainActivity.java
@@ -3,12 +3,26 @@ package com.hdjtdxwt.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.textView:{
+                Toast.makeText(this, "第一个textView", Toast.LENGTH_SHORT).show();
+            } break;
+        }
     }
 }
